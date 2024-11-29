1dcfcd0812b01ed3727553ea2486a37d9ecee700
Moeed-S-E <muhammadmoeed3737@outlook.com>
2024-11-29 20:34:03 +0500

    Major Release of My O-E-L

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <limits>
 
 using namespace std;
 
@@ -20,8 +19,52 @@ struct CartItem
     Item item;
     int quantity;
 };
-void displayItems(Item storeItems[], const int SIZE);
-int getValidQuantity(int availableStock);
+
+
+
+
+
+
+
+
+// Function to Display the Items
+void displayItems(Item storeItems[], const int SIZE)
+{
+    cout << "Available Items: \n";
+    for (size_t i = 0; i < SIZE; i++)
+    {
+        cout << storeItems[i].productID << ". " << storeItems[i].name << " ("
+             << storeItems[i].category << ") - $" << storeItems[i].price
+             << " (Stock: " << storeItems[i].stock << ")" << endl;
+    }
+}
+// Function to Check Available Stock
+int getValidQuantity(int availableStock)
+{
+    int quantity = 0;
+
+    while (true) // loop infinitly run
+    {
+        cout<<"Enter Quanity to add: ";
+        cin>>quantity;
+        if (cin.fail() || quantity <= 0 || quantity > availableStock)
+        {
+            cout << "Invalid quantity. Please enter a number between 1 and " << availableStock << ".\n";
+        }else
+        {
+            break;
+        }
+        
+        
+    }
+    
+
+    return quantity;
+}
+
+
+
+
 // Function to add item to the cart
 void addToCart(Item storeItems[], CartItem cart[], int& cartSize, int MAX_ITEMS, int MAX_CART){
     int itemNumber;
@@ -74,40 +117,42 @@ int main()
     CartItem cart[MAX_CART];
     int cartSize = 0;
 
-    return 0;
-}
+    CartItem cart[MAX_CART];
+    int cartSize = 0;  // To track the number of items in the cart
 
-// Function to Display the Items
-void displayItems(Item storeItems[], const int SIZE)
-{
-    cout << "Available Items: \n";
-    for (size_t i = 0; i < SIZE; i++)
-    {
-        cout << storeItems[i].productID << ". " << storeItems[i].name << " ("
-             << storeItems[i].category << ") - $" << storeItems[i].price
-             << " (Stock: " << storeItems[i].stock << ")" << endl;
-    }
-}
-// Function to Check Available Stock
-int getValidQuantity(int availableStock)
-{
-    int quantity = 0;
+    int choice;
 
-    while (true) // loop infinitly run
-    {
-        cout<<"Enter Quanity to add: ";
-        cin>>quantity;
-        if (cin.fail() || quantity <= 0 || quantity > availableStock)
-        {
-            cout << "Invalid quantity. Please enter a number between 1 and " << availableStock << ".\n";
-        }else
-        {
-            break;
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Display Items\n";
+        cout << "2. Add to Cart\n";
+        cout << "3. View Cart\n";
+        cout << "4. Checkout\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                displayItems(storeItems, MAX_ITEMS);
+                break;
+            case 2:
+                addToCart(storeItems, cart, cartSize, MAX_ITEMS, MAX_CART);
+                break;
+            case 3:
+                // viewCart(cart, cartSize); 
+                break;
+            case 4:
+                // checkout(cart, cartSize);
+                break;
+            case 5:
+                cout << "Exiting...\n";
+                break;
+            default:
+                cout << "Invalid choice. Try again.\n";
         }
-        
-        
-    }
-    
+    } while (choice != 5);
 
-    return quantity;
+
+    return 0;
 }
